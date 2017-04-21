fed41a19421e72fc68407e01d5e426954cc6f1ea
PhanHongNam <PhanHongNamDesktop>
2017-04-21 18:29:07 +0700

    test by Phan Hong Nam

--- src/bai_8/MyApp.java
+++ src/bai_8/MyApp.java
@@ -8,18 +8,18 @@ public class MyApp {
 		Thread thr1 = new Thread(obj);
 		thr1.start();
 		
-		MyThread2 thr2 = new MyThread2();
-		thr2.start();
+//		MyThread2 thr2 = new MyThread2();
+//		thr2.start();
 
-//		try {
-//			Thread.sleep(3000);// 5s
-//			synchronized (MyThread.objSync) {
-//				MyThread.objSync.notify();	
-//			}
-//			System.out.println("Notify");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		try {
+			Thread.sleep(3000);// 5s
+			synchronized (MyThread.objSync) {
+				MyThread.objSync.notify();	
+			}
+			System.out.println("Notify");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		// try{
 		// System.out.println("Start application");
